8e6c71a9ec83e236fc0d6b03ade316c5a7e63aaf
MomenSherif <momensherif.2019@gmail.com>
2021-01-29 21:28:40 +0200

    add sorting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import BasicTable from './components/BasicTable';
+import SortingTable from './components/SortingTable';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor eaque tempore quaerat eum beatae officiis aut tempora? Consequatur iure sunt sequi facilis magnam rem quae doloribus quasi pariatur, numquam possimus consequuntur natus illum, ducimus reiciendis enim ullam saepe itaque non.</h1>
+      <h1>Hello, World!</h1>
       <div className="table-container">
-        <BasicTable />
+        <SortingTable />
       </div>
     </div>
   );
