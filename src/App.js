7430501ac1a77ac4abdafde98bb1869829705c72
MomenSherif <momensherif.2019@gmail.com>
2021-02-02 22:35:08 +0200

    add selecting rows functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import BasicTable from './components/BasicTable';
 import SortingTable from './components/SortingTable';
 import FilteringTable from './components/FilteringTable';
 import PaginationTable from './components/PaginationTable';
+import RowSelection from './components/RowSelection';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
       <div className="table-container">
-        <PaginationTable />
+        <RowSelection />
       </div>
     </div>
   );
