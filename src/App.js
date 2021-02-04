6bc4543842321ca554dd22a9706d6bfee93a2bef
MomenSherif <momensherif.2019@gmail.com>
2021-02-04 23:48:53 +0200

    add column hiding functionality

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import FilteringTable from './components/FilteringTable';
 import PaginationTable from './components/PaginationTable';
 import RowSelection from './components/RowSelection';
 import ColumnOrder from './components/ColumnOrder';
+import ColumnHiding from './components/ColumnHiding';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
       <div className="table-container">
-        <ColumnOrder />
+        <ColumnHiding />
       </div>
     </div>
   );
